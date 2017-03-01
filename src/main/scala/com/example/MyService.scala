aa263119dedad4a7da4750d1e872094714a78e81
haulfield <haulfield@ex.ua>
2017-03-01 16:25:40 +0200

    improvements on classes api testing

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -19,6 +19,41 @@ class MyServiceActor extends Actor with MyService {
 
 case class Number(v2: Double, v3: Int, v4: Int)
 
+object CSVStore {
+  def path = "src/main/resources/"
+  def f1 = path + "f1.csv"
+  def f2 = path +"f2.csv"
+
+  def writeToCSV(writingStuff: Array[String], filename: String){
+    val pw = new PrintWriter(new File(filename))
+    writingStuff.foreach( x=>pw.write(x+","))
+    pw.close()
+  }
+  def csvFileToArray(filename: String) = io.Source.fromFile(filename).getLines().next().split(",")
+  def changeNumberInFile(filename: String, number: Double, position: Int): Unit ={
+    val f2 = csvFileToArray(filename)
+    f2(position) = number.toString
+    writeToCSV(f2, filename)
+  }
+}
+
+object Calculator {
+  def getParameter(number: Double) = if (number > 10) number - 10 else number
+
+  def changeParameter(f: Double, s: Double) = {
+    var returning = 0
+    val result = if ((f + s )< 10)
+    f + s + 10
+    else{
+      returning = 1
+      f + s
+    }
+    (returning, result)
+  }
+}
+
+
+
 object NumberJsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
   implicit val NumberFormats = jsonFormat3(Number)
 }
@@ -32,28 +67,19 @@ trait MyService extends HttpService {
           import NumberJsonSupport._
           var returning = 0
           entity(as[Number]) { number =>
-            val f1 = io.Source.fromFile("src/main/resources/f1.csv").getLines().next().split(",")
-            var f2 = io.Source.fromFile("src/main/resources/f2.csv").getLines().next().split(",")
-            val result = if ((f1(number.v3).toDouble + number.v2 )< 10)
-              f1(number.v3).toDouble+number.v2+10
-            else{
-              returning = 1
-              f1(number.v3).toDouble+number.v2
-            }
-            val pw = new PrintWriter(new File("src/main/resources/f2.csv"))
-            f2(number.v4) = result.toString
-            f2.foreach { x => pw.write(x+",")}
-            pw.close()
-
+            val f1 = CSVStore.csvFileToArray(CSVStore.f1)
+            var f2 = CSVStore.csvFileToArray(CSVStore.f2)
+            val (returning, result) = Calculator.changeParameter(f1(number.v3).toDouble, number.v2)
+            CSVStore.changeNumberInFile(CSVStore.f2, result, number.v4)
             complete {
                 <result>{returning}</result>
             }
           }
         } ~ get {
           parameters('v1.as[Int]) { (v1) =>
-            val f2 = io.Source.fromFile("src/main/resources/f2.csv").getLines().next().split(",")
+            val f2 = CSVStore.csvFileToArray(CSVStore.f2)
             val number = f2(v1).toDouble
-            val result = if (number > 10) number - 10 else number
+            val result = Calculator.getParameter(number)
             complete {
               <result>{result}</result>
             }
--- src/test/scala/com/example/CalculationsTest.scala
+++ src/test/scala/com/example/CalculationsTest.scala
@@ -2,19 +2,74 @@ package com.example
 
 import java.io.File
 import org.scalatest.FlatSpec
-/**
-  * Created by Дайдара on 21.02.2017.
-  */
 
+/*
+classes:
+CSVStore
+should
+a) return existing files from pathes
+b1)
+b) transform file to array of nums in correct way
+c) change number on specific position in file correctly
+Calculator
+should
+a) return parameter correctly
+b) set parameter correctly
+
+ */
 class CalculationsTest extends FlatSpec{
 
-  val path = "src/main/resources/"
   implicit class FileMonads(f: File) {
     def check = f.exists
+    def size = f.length()
+  }
+  def randomArray(size: Int) = {
+    var r = new scala.util.Random
+    1 to size map { _ =>
+      r.nextDouble().toString
+    }
+  }
+  def randomBoundedArray(size: Int, upperBound: Int) = {
+    var r = new scala.util.Random
+    1 to size map { _ =>
+      r.nextInt(upperBound).toDouble
+    }
   }
 
-  "Files f1 and f2 " should "exist" in {
-    assert(new File(path+"f1.csv").check)
-    assert(new File(path+"f2.csv").check)
+  "Calculator" should "return parameter correctly" in {
+    val bigArray = randomBoundedArray(100, 19)
+    bigArray.foreach(x=>
+      assert(
+        Calculator.getParameter(x)<=10))
+  }
+  it should "set parameter correctly" in {
+    val bigArray = randomBoundedArray(100, 9)
+    val bigSecondArray = randomBoundedArray(100, 9)
+    (0 to 99).foreach(x => {
+      val (_, result) = Calculator.changeParameter(bigArray(x), bigSecondArray(x))
+      assert(result < 20 && result >= 10)
+    }
+    )
+
+  }
+
+  var testArray = randomArray(15).toArray
+  "CVSStore" should "return exists files from pathes" in {
+    assert(new File(CSVStore.f1).check)
+    assert(new File(CSVStore.f2).check)
+  }
+  it should "write array to file" in {
+    CSVStore.writeToCSV(testArray, "temp.csv")
+    assert(new File("temp.csv").size > 0)
+  }
+  it should "transform csv file to double array" in {
+    val isTestArray = CSVStore.csvFileToArray("temp.csv")
+    assert(testArray.sameElements(isTestArray))
+  }
+  it should "change number on specific position" in {
+    CSVStore.changeNumberInFile("temp.csv", 127.0, 2)
+    val fileArray = CSVStore.csvFileToArray("temp.csv")
+    testArray(2) = 127.0.toString
+    assert(testArray.sameElements(fileArray))
   }
 }
